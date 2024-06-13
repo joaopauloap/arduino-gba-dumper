acaaac5929b34987ce15bb379d37bb9f4312cf78
João Paulo de Araújo Porto <joaopaulo.porto@foursys.com.br>
2024-06-13 00:57:38 -0300

    update

--- arduino-gba-dumper.ino
+++ arduino-gba-dumper.ino
@@ -1,25 +1,91 @@
-int RD = 1;
-int WR = 4;
-int CS = 2;
-int CS2 = 3;
+//cartridge pins
+int RD = 4;   //control bit
+int CS2 = 2;  //switch ROM/RAM
+int CS = 3;   //switch Address/Data
+//int WR = 5;   //Write strobe
 
-int DS = 10;
-int CLOCK = 11;
-int LATCH = 12;
+//shift register 74HC595 pins
+int CLOCK = A0;
+int LATCH = A1;
+int DATA = A2;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(DS, OUTPUT);
-  pinMode(CLOCK, OUTPUT); 
-  pinMode(LATCH, OUTPUT); 
-
+  pinMode(CLOCK, OUTPUT);
+  pinMode(LATCH, OUTPUT);
+  pinMode(DATA, OUTPUT);
   pinMode(RD, OUTPUT);
   pinMode(CS, OUTPUT);
+  pinMode(CS2, OUTPUT);
+  //pinMode(WR, OUTPUT);
+
+  //Data bus
+  for (int pin = 5; pin <= 12; pin++) {
+    pinMode(pin, INPUT_PULLUP);
+  }
+
   digitalWrite(RD, HIGH);
-  digitalWrite(WR, HIGH);
-  digitalWrite(CS, LOW);
+  digitalWrite(CS, HIGH);
+  digitalWrite(CS2, HIGH);
+  //digitalWrite(WR, HIGH);
+}
+
+void latchRAMAddress(unsigned int addr) {
+  digitalWrite(LATCH, LOW);
+  shiftOut(DATA, CLOCK, MSBFIRST, addr >> 8);
+  shiftOut(DATA, CLOCK, MSBFIRST, addr);
+  digitalWrite(LATCH, HIGH);
+}
+
+
+
+unsigned int readRAMDataBus() {
+  uint8_t portd_bits = ~PIND & 0b11100000;
+  portd_bits >>= 5;
+  uint8_t portb_bits = ~PINB & 0b00011111;
+  return (portb_bits << 3) | portd_bits;
+}
+
+void printByte(int value) {
+  // char buffer[3];
+  // sprintf(buffer, "%02X", value);
+  // Serial.print(buffer);
+  // Serial.print(" ");
+  Serial.write(value);
+}
+
+
+void dumpSave() {
+  // Serial.println("Dumping GBA Save...");
+  digitalWrite(CS2, LOW);
+  delay(100);
+
+  for (unsigned int addr = 0x00; addr < 0xFFFF; addr++) {
+
+    latchRAMAddress(addr);
+    digitalWrite(RD, LOW);
+    delayMicroseconds(10);
+    digitalWrite(RD, HIGH);
+
+    int b = readRAMDataBus();
+
+    if (addr % 32 == 0) {
+      Serial.println("");
+    }
+    printByte(b);
+  }
+
   digitalWrite(CS2, HIGH);
 }
 
+
+
 void loop() {
+  while (Serial.available() == 0)
+    ;
+  char op = Serial.read();
+  if (op == '2') {
+    dumpSave();
+    // Serial.println("END OF DUMP");
+  }
 }
---
+++ dumper.py
@@ -0,0 +1,103 @@
+import serial
+import time
+
+port = 'COM3' 
+baudrate = 9600
+timeout = 1 
+dumpTimeout = 2 
+
+# Conexão serial
+try:
+    ser = serial.Serial(port, baudrate, timeout=timeout)
+except serial.SerialException as e:
+    print(f'Error opening port {port}: {e}')
+    exit()
+
+
+def receiveDump(isCreateFile):
+    data = b''
+    timeout_counter = time.time()
+
+    if isCreateFile:
+        print('Dumping, please await...')
+        fileName = 'dump.txt'
+        file = open(fileName, 'wb')
+
+    try:
+        while True:
+            byte = ser.read()
+            if byte:
+                if isCreateFile:
+                    file.write(byte)  
+                else:
+                    data += byte  
+                timeout_counter = time.time()
+            else:
+                if time.time() - timeout_counter > dumpTimeout:
+                    break
+    except serial.SerialException as e:
+        print(f'Error: {e}')
+    finally:
+        if isCreateFile:
+            file.close() 
+            print(f'Done. Saved at "{fileName}".')
+        else:
+            print(data.decode())
+
+
+def print_game_title():
+    try:
+        print('Getting game title...')
+        ser.write(b'0')
+    except serial.SerialException as e:
+        print(f'Error: {e}')
+        ser.close()
+        exit()
+    receiveDump(False)
+
+def dump_rom():
+    try:
+        ser.write(b'1')
+    except serial.SerialException as e:
+        print(f'Error: {e}')
+        ser.close()
+        exit()
+    receiveDump(True)
+
+def dump_ram():
+    try:
+        ser.write(b'2')
+    except serial.SerialException as e:
+        print(f'Error: {e}')
+        ser.close()
+        exit()
+    receiveDump(True)
+
+def write_ram():
+    print("WIP")
+
+while True:
+    print(f"\n=== GBA Game Dumper ({port}) ===")
+    print("0 - Print game title")
+    print("1 - Dump ROM")
+    print("2 - Dump RAM")
+    print("3 - Write RAM")
+    print("4 - Exit")
+    op = input("\n>")
+    
+    if op == '0':
+        dumpTimeout = 2
+        #print_game_title()
+    elif op == '1':
+        #dump_rom()
+        print("")
+    elif op == '2':
+        dumpTimeout = 5   
+        dump_ram()
+    elif op == '3':
+       #write_ram()
+       print("")
+    elif op == '4':
+        break
+    else:
+        print("Invalid op.")
