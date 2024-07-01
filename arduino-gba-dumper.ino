b52f27b8883de6300ff2fe7f6513b06486dca118
João Paulo de Araújo Porto <joaopaulo.porto@foursys.com.br>
2024-07-01 20:59:13 -0300

    try read flash send read memory commands

--- arduino-gba-dumper.ino
+++ arduino-gba-dumper.ino
@@ -32,16 +32,33 @@ void setup() {
   digitalWrite(WR, HIGH);
 }
 
-void latchAddress(unsigned long addr) {
+void setAddress(unsigned long addr) {
   digitalWrite(LATCH, LOW);
   shiftOut(DATA, CLOCK, MSBFIRST, (addr >> 8));
   shiftOut(DATA, CLOCK, MSBFIRST, addr);
   digitalWrite(LATCH, HIGH);
 }
 
-
 unsigned int readDataBus() {
   return ((PINB & 0b00000011) << 6) | ((PIND & 0b11111100) >> 2);
+}
+
+void writeDataBus(unsigned int value) {
+  for (int pin = 2; pin <= 9; pin++) {
+    pinMode(pin, OUTPUT);
+  }
+  PORTD = (PORTD & 0b00000011) | (value & 0b11111100);  // Configura bits 2-7 de PORTD
+  PORTB = (PORTB & 0b11111100) | (value & 0b00000011);  // Configura bits 0-1 de PORTB
+}
+
+void sendCommand(uint32_t address, byte value) {
+  setAddress(address);
+  writeDataBus(value);
+
+  delayMicroseconds(1);
+  digitalWrite(WR, LOW);
+  delayMicroseconds(1);
+  digitalWrite(WR, HIGH);
 }
 
 void printByte(int value) {
@@ -53,13 +70,39 @@ void printByte(int value) {
 
 void dumpSave(int size) {
   // Serial.println("Dumping GBA Save...");
-  unsigned long lastAddr = (unsigned long) size * 1024;
+  unsigned long lastAddr = (unsigned long)size * 1024;
   digitalWrite(CS, HIGH);  //Disable ROM
   digitalWrite(CS2, LOW);
   delay(100);
 
   for (unsigned long addr = 0; addr < lastAddr; addr++) {
-    latchAddress(addr);
+    setAddress(addr);
+    digitalWrite(RD, LOW);
+    delayMicroseconds(1);
+    digitalWrite(RD, HIGH);
+    int b = readDataBus();
+    Serial.write(b);
+  }
+  digitalWrite(CS2, HIGH);
+}
+
+void dumpRAM(int size) {
+  unsigned long lastAddr = (unsigned long)size * 1024;
+  digitalWrite(CS, HIGH);  //Disable ROM
+  digitalWrite(CS2, LOW);
+  delay(100);
+
+  sendCommand(0x5555, 0xaa);
+  sendCommand(0x2aaa, 0x55);
+  sendCommand(0x5555, 0xb0);
+  sendCommand(0x0000, 0x00);
+
+  for (int pin = 2; pin <= 9; pin++) {
+    pinMode(pin, INPUT_PULLUP);
+  }
+  
+  for (unsigned long addr = 0; addr < lastAddr; addr++) {
+    setAddress(addr);
     digitalWrite(RD, LOW);
     delayMicroseconds(1);
     digitalWrite(RD, HIGH);
@@ -70,14 +113,13 @@ void dumpSave(int size) {
 }
 
 
-
 void loop() {
   if (Serial.available() > 0) {
     char op = Serial.read();
 
     //Dump save option
     if (op == '2') {
-      dumpSave(128);
+      dumpRAM(128);
     }
   }
 }
